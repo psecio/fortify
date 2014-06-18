16cbf92df49cbef65cd346bbaf508f0778865dd6
Chris Cornutt <enygma@phpdeveloper.org>
2014-06-18 11:44:40 -0500

    Abstracting the Auth* classes out as Enforcers, moving Decider methods to parent class

--- src/Psecio/Fortify/Authorize.php
+++ src/Psecio/Fortify/Authorize.php
@@ -2,27 +2,7 @@
 
 namespace Psecio\Fortify;
 
-class Authorize
+class Authorize extends \Psecio\Fortify\Enforcer
 {
-	private $decider;
 
-	public function __construct(\Psecio\Fortify\Decider $decider)
-	{
-		$this->setDecider($decider);
-	}
-
-	public function setDecider(\Psecio\Fortify\Decider $decider)
-	{
-		$this->decider = $decider;
-		return $this;
-	}
-	public function getDecider()
-	{
-		return $this->decider;
-	}
-
-	public function evaluate(\Psecio\Fortify\Subject $subject)
-	{
-		return $this->getDecider()->evaluate($subject);
-	}
 }

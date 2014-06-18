922fbf73a9619e5a7b2ee4146142da0728d61e4a
Chris Cornutt <enygma@phpdeveloper.org>
2014-06-18 11:46:23 -0500

    adding docblocks to Subject class

--- src/Psecio/Fortify/Subject.php
+++ src/Psecio/Fortify/Subject.php
@@ -4,8 +4,17 @@ namespace Psecio\Fortify;
 
 class Subject
 {
+	/**
+	 * Current object properties
+	 * @var array
+	 */
 	private $properties = array();
 
+	/**
+	 * Create the object and initialize properties if given
+	 *
+	 * @param array $properties
+	 */
 	public function __construct(array $properties = array())
 	{
 		if (!empty($properties)) {
@@ -15,17 +24,35 @@ class Subject
 		}
 	}
 
+	/**
+	 * Set a property on the current object
+	 *
+	 * @param string $name Name of property
+	 * @param mixed $value Value for property
+	 * @return \Psecio\Fortify\Subject instance
+	 */
 	public function setProperty($name, $value)
 	{
 		$this->properties[$name] = $value;
 		return $this;
 	}
 
+	/**
+	 * Get the full set of current properties
+	 *
+	 * @return array Property set
+	 */
 	public function getProperties()
 	{
 		return $this->properties;
 	}
 
+	/**
+	 * Get one specific property
+	 *
+	 * @param string $name
+	 * @return mixed If the property is not set, returns null
+	 */
 	public function getProperty($name)
 	{
 		return (isset($this->properties[$name]))

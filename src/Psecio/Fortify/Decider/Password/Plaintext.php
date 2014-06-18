d09336f231affd2667f8c12eda91da5e9b23cb99
Chris Cornutt <enygma@phpdeveloper.org>
2014-06-18 12:09:05 -0500

    Adding docblock to Plaintext evaluate method and checking other password for null

--- src/Psecio/Fortify/Decider/Password/Plaintext.php
+++ src/Psecio/Fortify/Decider/Password/Plaintext.php
@@ -4,12 +4,20 @@ namespace Psecio\Fortify\Decider\Password;
 
 class Plaintext extends \Psecio\Fortify\Decider\Password
 {
+	/**
+	 * Test the "password" value on the Subject against the
+	 *   plaintext value given
+	 *
+	 * @param \Psecio\Fortify\Subject $subject Subject for evaluation
+	 * @throws \InvalidArgumentException If password is not defined
+	 * @return boolean Pass/fail status
+	 */
 	public function evaluate($subject)
 	{
 		$password = $subject->getProperty('password');
 		$compare = $this->getData('password');
 
-		if ($compare == null) {
+		if ($compare == null || $password == null) {
 			throw new \InvalidArgumentException('Password not defined!');
 		}
 
